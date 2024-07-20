68e8aee057c614a1888a5a080b6e438e7c34416d
Dhyan Tanna <79247409+D99-1@users.noreply.github.com>
2024-07-20 16:41:16 +1000

    Update index.js

--- art/Burger-Dhyan99/index.js
+++ art/Burger-Dhyan99/index.js
@@ -2,23 +2,36 @@ setDocDimensions(800, 400);
 
 
 const flagTurtle = new bt.Turtle();
-flagTurtle.jump([100, 200]);
+flagTurtle.jump([236, 299]);
 flagTurtle.right(0);
-flagTurtle.forward(563);
-flagTurtle.right(90);
-flagTurtle.forward(141);
-flagTurtle.right(90);
-flagTurtle.forward(563);  
-flagTurtle.right(90);
-flagTurtle.forward(148); 
-flagTurtle.right(270);
-flagTurtle.forward(10);
-flagTurtle.right(270);
-flagTurtle.forward(204);
-flagTurtle.right(270);
-flagTurtle.forward(10);
-flagTurtle.right(270);
-flagTurtle.forward(156);
+flagTurtle.arc(1,690)
+flagTurtle.arc(-14,590)
+flagTurtle.forward(0);
+flagTurtle.right(-21)
+flagTurtle.arc(-11,1139)
+flagTurtle.right(-377)
+flagTurtle.arc(7,962);
+flagTurtle.right(-215)
+flagTurtle.forward(80);
+flagTurtle.right(-126)
+flagTurtle.forward(80);
+flagTurtle.right(-227)
+flagTurtle.forward(80);
+flagTurtle.arc(-49,116)
+flagTurtle.forward(259);
+flagTurtle.arc(25,273)
+flagTurtle.right(302)
+flagTurtle.forward(106)
+flagTurtle.right(439)
+flagTurtle.forward(16)
+flagTurtle.right(99)
+flagTurtle.forward(281)
+flagTurtle.right(449)
+flagTurtle.forward(8)
+flagTurtle.right(456)
+flagTurtle.forward(106)
+
+
 
 let flagPolylines = flagTurtle.lines();
 
